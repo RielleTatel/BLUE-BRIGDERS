c64ceecf7916a78badfe202742b0d4028871f315
Qhaleed <reslykadiri@gmail.com>
2024-11-20 23:14:55 +0800

    header update

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,16 @@
 import { Header } from "./mainContent.styled";
 
-export const PagesHeader = () => { 
+export const PagesHeader = () => {
 
-    return (
-      <div> 
-          <Header> 
-            <h1> Blue Brigde </h1>
-            <p> Donation Volunteer Goods </p>
-          </Header>
-      </div>
-    );
-  };
-  
+  return (
+    <div>
+      <Header>
+        <h1> Blue Brigde </h1>
+        {/* Will edit the header */}
+        <p> Donation Volunteer Goods </p>
+      </Header>
+    </div>
+  );
+};
+
 export default PagesHeader;
